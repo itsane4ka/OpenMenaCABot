f3b2573fe9549f69e97766b28ae3cdc7e3553791
олександр кац <katsaleksandr424242@gmail.com>
2023-06-17 23:09:25 +0200

    Merged files

--- main.py
+++ main.py
@@ -1,18 +1,63 @@
 import telebot
-import re
 from telebot import types
 from messages import Messages
 import random
+import requests
 
 # Создаем экземпляр бота
 bot = telebot.TeleBot('5839845850:AAHtIvxrAMgxAmI8JR4lSJEUXbVUYiLchj0')
-
+url_ca = 'https://docs.google.com/forms/d/e/1FAIpQLSf2TDJR1j6BTLtB0sSsi286q0qR2h0t4Aq8Fx05Z_o8EjkNeQ/formResponse'
+url_mena = 'https://docs.google.com/forms/d/e/1FAIpQLSdtXAzG6D39SPEFBtk17dFo_cgrniNCdP05tf67nV1-BNip-Q/formResponse'
 mes = Messages()
 
-channel_link = "https://t.me/+pgSMhr10-R0yOTIy"
-
+channel_link = "https://t.me/testbot123da"
+channel_id = [-1001930406351]
+channel_open_california_link = "link to channel OpenCA"
+channel_open_california_chat_link = 'link to chat OpenCA'
+channel_open_mena_link = 'link to channel OpenMENA'
+channel_open_mena_chat_link = 'link to chat OpenMENA'
 subscriptions = {}
 
+user_state_ca, user_state_mena, user_data_ca, user_data_mena, user_data_ca_for_post, user_data_mena_for_post = {}, {}, {}, {}, {}, {},
+
+fields_for_ca = ['Имя Фамилия', 'Телефон для связи', 'Ваш Telegram', 'Email', 'В какой стране Вы живете?',
+                 'Опишите деятельность Вашей компании?', 'Регион присутствия (в том числе планируемый) Вашей компании?',
+                 'Как называется Ваша компания?', 'Ссылка на сайт компании', 'Годовой оборот Вашей компании?',
+                 'Количество человек в Вашей команде?',
+                 'Дополнительные направления бизнеса, которые Вы развиваете?',
+                 'Вы хотите стать спикером на мероприятиях OpenCA?', 'На какие темы Вы хотите выступать?',
+                 'Хотели бы Вы стать партнером OpenCA?',
+                 'Цель участия в OpenCA', 'Какой из форматов встреч нравится и актуален для вас?']
+
+fields_for_mena = ['Имя Фамилия', 'Телефон для связи', 'Ваш Telegram', 'Email', 'В какой стране Вы живете?',
+                   'Опишите деятельность Вашей компании?',
+                   'Регион присутствия (в том числе планируемый) Вашей компании?',
+                   'Как называется Ваша компания?', 'Ссылка на сайт компании', 'Годовой оборот Вашей компании?',
+                   'Количество человек в Вашей команде?',
+                   'Дополнительные направления бизнеса, которые Вы развиваете?',
+                   'Вы хотите стать спикером на мероприятиях OpenMENA?', 'На какие темы Вы хотите выступать?',
+                   'Хотели бы Вы стать партнером OpenMENA?',
+                   'Цель участия в OpenMENA', 'Какой из форматов встреч нравится и актуален для вас?']
+
+form_entry = [
+    'entry.91462916',
+    'entry.630533396',
+    'entry.2039931063',
+    'entry.1318559028',
+    'entry.1254544746',
+    'entry.191036165',
+    'entry.854259575',
+    'entry.1448253998',
+    'entry.729755307',
+    'entry.601340499',
+    'entry.292760203',
+    'entry.1874047276',
+    'entry.1566950839',
+    'entry.104522551',
+    'entry.281659691',
+    'entry.2132372300',
+    'entry.1885107982',
+]
 
 # Обработчик команды /start
 @bot.message_handler(commands=['start'])
@@ -113,12 +158,151 @@ def handle_button_click(message):
     elif text == "Связаться с менеджером":
         send_contact_message(chat_id)
 
-    elif text == "Подать заявку":
-        #TODO
-        pass
-
     elif text == "На главную":
         send_subscribed_message(chat_id)
+
+
+    #vlad code
+
+    elif text == "Подать заявку":
+        keyboard = types.ReplyKeyboardMarkup(row_width=1, resize_keyboard=True)
+        button = types.KeyboardButton(text="Заполнить анкету")
+        button1 = types.KeyboardButton(text="На главную")
+        keyboard.add(button, button1)
+        bot.send_message(chat_id, mes.message7(), reply_markup=keyboard)
+
+    elif text == "Стать участником OpenCA":
+        keyboard = types.ReplyKeyboardMarkup(row_width=1, resize_keyboard=True)
+        button = types.KeyboardButton(text="Заполнить анкету OpenCA")
+        button1 = types.KeyboardButton(text="На главную")
+        keyboard.add(button, button1)
+        bot.send_message(chat_id, mes.message7(), reply_markup=keyboard)
+
+    elif text == "Заполнить анкету OpenCA":
+        user_state_ca[chat_id] = 0
+        user_data_ca[chat_id] = {}
+        ask_question_ca(message)
+
+    elif text == "Заполнить анкету":
+        user_state_mena[chat_id] = 0
+        user_data_mena[chat_id] = {}
+        ask_question_mena(message)
+
+    elif chat_id in user_state_ca:
+        if text == "Пропустить":
+            user_data_ca[chat_id][fields_for_ca[user_state_ca[chat_id]]] = ""
+            user_data_ca_for_post[form_entry[user_state_ca[chat_id]]] = ""
+        else:
+            user_data_ca[chat_id][fields_for_ca[user_state_ca[chat_id]]] = text
+            user_data_ca_for_post[form_entry[user_state_ca[chat_id]]] = text
+        user_state_ca[chat_id] += 1
+        ask_question_ca(message)
+
+    elif chat_id in user_state_mena:
+        if text == "Пропустить":
+            user_data_mena[chat_id][fields_for_mena[user_state_mena[chat_id]]] = ""
+            user_data_mena_for_post[form_entry[user_state_mena[chat_id]]] = ""
+        else:
+            user_data_mena[chat_id][fields_for_mena[user_state_mena[chat_id]]] = text
+            user_data_mena_for_post[form_entry[user_state_mena[chat_id]]] = text
+        user_state_mena[chat_id] += 1
+        ask_question_mena(message)
+
+
+
+def ask_question_ca(message):
+    chat_id = message.chat.id
+
+    if user_state_ca[chat_id] >= len(fields_for_ca):
+        bot.send_message(chat_id, 'Спасибо за ваше время. Мы получили все ваши ответы.')
+        keyboard = types.ReplyKeyboardMarkup(row_width=1, resize_keyboard=True)
+        button = types.KeyboardButton(text="На главную")
+        keyboard.add(button)
+        response = requests.post(url_ca, data=user_data_ca_for_post)
+        user_data_ca_for_post.clear()
+        if response.status_code == 200:
+            print('Форма успешно отправлена')
+        else:
+            print('Произошла ошибка при отправке формы')
+        bot.send_message(chat_id,
+                         f'Форма успешно отправлена, вы можете присоединиться к нашему сообществу по этой ссылке: {channel_open_california_chat_link}',
+                         reply_markup=keyboard)
+
+        del user_state_ca[chat_id]
+    elif fields_for_ca[user_state_ca[chat_id]] == 'Хотели бы Вы стать партнером OpenCA?':
+        keyboard = types.ReplyKeyboardMarkup(row_width=1, resize_keyboard=True)
+        keyboard.add('Да', 'Нет', 'Не знаю, что мне это даст')
+        bot.send_message(chat_id, fields_for_ca[user_state_ca[chat_id]], reply_markup=keyboard)
+    elif fields_for_ca[user_state_ca[chat_id]] == 'Какой из форматов встреч нравится и актуален для вас?':
+        keyboard = types.ReplyKeyboardMarkup(row_width=1, resize_keyboard=True)
+        keyboard.add('Pitch Day и панельная дискуссия с инвесторами',
+                     'Проверенная информация, как развиваться в California',
+                     'Практика для Вашего бизнеса на стратегических сессиях',
+                     'Погружение через бизнес-поездки по региону')
+        bot.send_message(chat_id, fields_for_ca[user_state_ca[chat_id]], reply_markup=keyboard)
+    elif fields_for_ca[user_state_ca[chat_id]] == 'Телефон для связи':
+        keyboard = types.ReplyKeyboardMarkup(row_width=1, resize_keyboard=True)
+        button_phone = types.KeyboardButton(text="Отправить номер телефона", request_contact=True)
+        keyboard.add(button_phone)
+        bot.send_message(chat_id, "Пожалуйста, отправьте ваш номер телефон", reply_markup=keyboard)
+    elif fields_for_ca[user_state_ca[chat_id]] == 'Количество человек в Вашей команде?':
+        keyboard = types.ReplyKeyboardMarkup(row_width=1, resize_keyboard=True)
+        keyboard.add('Пропустить')
+        bot.send_message(chat_id, fields_for_ca[user_state_ca[chat_id]], reply_markup=keyboard)
+    elif fields_for_ca[user_state_ca[chat_id]] == 'На какие темы Вы хотите выступать?':
+        keyboard = types.ReplyKeyboardMarkup(row_width=1, resize_keyboard=True)
+        keyboard.add('Пропустить')
+        bot.send_message(chat_id, fields_for_ca[user_state_ca[chat_id]], reply_markup=keyboard)
+    else:
+        keyboard = types.ReplyKeyboardRemove()
+        bot.send_message(chat_id, fields_for_ca[user_state_ca[chat_id]], reply_markup=keyboard)
+
+def ask_question_mena(message):
+    chat_id = message.chat.id
+
+    if user_state_mena[chat_id] >= len(fields_for_mena):
+        bot.send_message(chat_id, 'Спасибо за ваше время. Мы получили все ваши ответы.')
+        keyboard = types.ReplyKeyboardMarkup(row_width=1, resize_keyboard=True)
+        button = types.KeyboardButton(text="На главную")
+        keyboard.add(button)
+        response = requests.post(url_mena, data=user_data_mena_for_post)
+        user_data_mena_for_post.clear()
+        if response.status_code == 200:
+            print('Форма успешно отправлена')
+        else:
+            print('Произошла ошибка при отправке формы')
+        bot.send_message(chat_id,
+                         f'Форма успешно отправлена, вы можете присоединиться к нашему сообществу по этой ссылке: {channel_open_mena_chat_link}',
+                         reply_markup=keyboard)
+
+        del user_state_mena[chat_id]
+    elif fields_for_mena[user_state_mena[chat_id]] == 'Хотели бы Вы стать партнером OpenMENA?':
+        keyboard = types.ReplyKeyboardMarkup(row_width=1, resize_keyboard=True)
+        keyboard.add('Да', 'Нет', 'Не знаю, что мне это даст')
+        bot.send_message(chat_id, fields_for_mena[user_state_mena[chat_id]], reply_markup=keyboard)
+    elif fields_for_mena[user_state_mena[chat_id]] == 'Какой из форматов встреч нравится и актуален для вас?':
+        keyboard = types.ReplyKeyboardMarkup(row_width=1, resize_keyboard=True)
+        keyboard.add('Pitch Day и панельная дискуссия с инвесторами',
+                     'Проверенная информация, как развиваться в MENA',
+                     'Практика для Вашего бизнеса на стратегических сессиях',
+                     'Погружение через бизнес-поездки по региону')
+        bot.send_message(chat_id, fields_for_mena[user_state_mena[chat_id]], reply_markup=keyboard)
+    elif fields_for_mena[user_state_mena[chat_id]] == 'Телефон для связи':
+        keyboard = types.ReplyKeyboardMarkup(row_width=1, resize_keyboard=True)
+        button_phone = types.KeyboardButton(text="Отправить номер телефона", request_contact=True)
+        keyboard.add(button_phone)
+        bot.send_message(chat_id, "Пожалуйста, отправьте ваш номер телефон", reply_markup=keyboard)
+    elif fields_for_mena[user_state_mena[chat_id]] == 'Количество человек в Вашей команде?':
+        keyboard = types.ReplyKeyboardMarkup(row_width=1, resize_keyboard=True)
+        keyboard.add('Пропустить')
+        bot.send_message(chat_id, fields_for_mena[user_state_mena[chat_id]], reply_markup=keyboard)
+    elif fields_for_mena[user_state_mena[chat_id]] == 'На какие темы Вы хотите выступать?':
+        keyboard = types.ReplyKeyboardMarkup(row_width=1, resize_keyboard=True)
+        keyboard.add('Пропустить')
+        bot.send_message(chat_id, fields_for_mena[user_state_mena[chat_id]], reply_markup=keyboard)
+    else:
+        keyboard = types.ReplyKeyboardRemove()
+        bot.send_message(chat_id, fields_for_mena[user_state_mena[chat_id]], reply_markup=keyboard)
 
 
 # Send the analysis info
@@ -305,34 +489,6 @@ def send_contact_message(chat_id):
 
     # Send the contact
     bot.send_contact(chat_id, phone_number, first_name, last_name)
-
-
-def get_channel_user_ids(channel_link):
-    print(channel_link)
-
-    # Extract the channel username or ID from the link
-    match = re.search(r'(?:https?://)?(?:www\.)?t\.me/(joinchat/)?(?:invite/)?([a-zA-Z0-9_-]+)', channel_link)
-    if match:
-        channel_identifier = match.group(2)
-
-        # Get the chat ID
-        chat_id = None
-        try:
-            chat_id = int(channel_identifier)
-        except ValueError:
-            pass
-
-        # Get the user ID
-        user_id = None
-        if not chat_id:
-            try:
-                user_id = int(channel_identifier)
-            except ValueError:
-                pass
-
-        return chat_id, user_id
-
-    return None, None
 
 
 # Запускаем бота
